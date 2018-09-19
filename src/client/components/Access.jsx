06a376a7e2c8dbcfbd689ea9e662dadbda060001
John <darkneter@gmail.com>
2018-09-19 13:42:21 -0700

    Got the recover passphrase.

--- src/client/components/Access.jsx
+++ src/client/components/Access.jsx
@@ -10,6 +10,7 @@ class Access extends React.Component {
             alias:'guest',//input variable
             passphrase:'guest',//input variable
             aliaskooup:'guest',//input variable
+            lookpublickey:'',
             question1:'test',//input variable
             question2:'test',//input variable
             hint:'',//input variable
@@ -21,10 +22,28 @@ class Access extends React.Component {
         this.setState({alias: event.target.value});
     }
     //input handler
-    handlelookalias(event) {
+    async handlelookalias(event) {
         this.setState({aliaskooup: event.target.value});
-        
 
+        let gun = this.$gun;
+        let alias = await gun.get('~@'+event.target.value).then(); //broken or remove?
+        console.log(alias);
+        if(!alias){
+            //this.statusdisplay = 'Not Found!';
+            console.log('Not Found!');
+            return;
+        }else{
+            //this.statusdisplay = 'Found!';
+            console.log('Found!');
+        }
+        let publickey = '';
+        for(var obj in alias){
+            console.log(obj);
+            publickey = obj;
+        }
+        publickey = publickey.substring(1,publickey.length);//remove ~ string begin
+        this.setState({lookpublickey:publickey});
+        //this.publickey = publickey;
 
     }
     //input handler
@@ -43,6 +62,56 @@ class Access extends React.Component {
     handleChangeHint(event) {
         this.setState({hint: event.target.value});
     }
+
+    async clickRecover(){
+        let gun = this.$gun;
+        //let user = this.$gun.user();
+        let alias = (this.state.aliaskooup || '').trim(); //get alias input
+        let q1 =  (this.state.question1 || '').trim(); //get q1 input
+        let q2 = (this.state.question2 || '').trim(); //get q2 input
+        //console.log('get forgot hint');
+        if(!alias|| !q1 || !q2 ){
+            console.log('Empty!');
+            return;
+        }
+        //console.log(alias);
+        //Make sure the alias and public key are working
+        alias = await gun.get('~@'+alias).then();//look for hash id
+        let publickey = '';
+        for(var obj in alias){
+            //console.log(obj);
+            publickey = obj;
+        }
+        publickey = publickey.substring(1,publickey.length);
+        if(!publickey){
+            return;
+        }
+        //let publickey = this.publickey;
+        let to = this.$gun.user(publickey);
+        let who = await to.get('alias').then();
+        //console.log(who);
+        if(!who){
+            //console.log(who);
+            console.log('Not Alias!');
+            return;
+        }
+        //let hint = await gun.get('alias/'+alias).map().get('hint').then();//get hash hint string
+        let hint = hint = await to.get('hint').then();
+        let dec = await Gun.SEA.work(q1,q2);//get q1 and q2 string to key hash
+        hint = await Gun.SEA.decrypt(hint,dec);//get hint and key decrypt message
+        //console.log('hint',hint);
+        if(hint !=null){//check if hint is string or null
+            //$('#hint').val(hint);//get hint and set input value
+            hint = hint;
+        }else{
+            //$('#hint').val('Fail Decrypt!');//if null set input to message user.
+            hint = 'Fail Decrypt!';
+        }
+        this.setState({hint:hint});
+        console.log("end checked...");
+    }
+
+
 
     //render signin html
     DrawSignIn(){
@@ -186,9 +255,7 @@ class Access extends React.Component {
         });
     }
 
-    clickRecover(){
-
-    }
+    
 
     //go to sign up
     clickSignup(){
